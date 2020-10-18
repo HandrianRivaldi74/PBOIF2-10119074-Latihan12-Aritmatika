73941b12c3bead45614aa7ca81620a59ffdfb633
HandrianRivaldi74 <handrianr28@gmail.com>
2020-10-18 14:54:10 +0700

    Latihan12

---
+++ src/pboif2_10119074_latihan12/PBOIF2_10119074_Latihan12.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2_10119074_latihan12;
+
+/**
+ *
+ * @author Acer
+ * NAMA     : Handrian Rivaldi
+ * KELAS    : PBOIF2
+ * NIM      : 10119074
+ * Deskrifsi : Aritmatika
+ */
+public class PBOIF2_10119074_Latihan12 {
+
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c = 25;
+        int d = 25;
+        System.out.println("a + b = " + (a + b));
+        System.out.println("a - b = " + (a - b));
+        System.out.println("a * b = " + (a * b));
+        System.out.println("b / a = " + (b / a));
+        System.out.println("b % a = " + (b % a));
+        System.out.println("c % a = " + (c % a));
+        System.out.println("a++ = " + (a++));
+        System.out.println("b-- = " + (a--));
+        System.out.println("d++ = " + (d++));
+        System.out.println("++d = " + (++d));
+        
+                
+    }
+    
+}
